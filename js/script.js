21e1b4246219d53d11b0ccbac0762f26ecbb179a
Joseph-Simon <36781112+Joseph-Simon@users.noreply.github.com>
2018-03-23 13:09:43 -0400

    Second Function added
    Added next function to print the Random Quote to the page.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-function getRandomQuote(){
+
+
+function getRandomQuote(randomQuote){
     var randomQuote = quotes[Math.floor( Math.random() *  quotes.length )];
     return randomQuote;
 }
+
+function printQuote(){
+    randomQuote = getRandomQuote();
+    html = '<p class="quote">' + randomQuote.quote + '</p>';
+    html += '<p class="source">' + randomQuote.source;
+    if (randomQuote.citation) {
+      html += '<span class="citation">' + [randomQuote.citation] + '</span>';
+    }
+    if (randomQuote.year) {
+      html += '<span class="year">' + [randomQuote.year] + '</span>';
+    }
+    if (randomQuote.tag) {
+      html +='<span class="tag">' + [randomQuote.tag] + '</span>';
+    }
+    var div = document.getElementById('quote-box').innerHTML = html;
+    return html;
+  }
+
+
+
+
