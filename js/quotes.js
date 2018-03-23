fe1b20928ecf3214830df920b6cbb7f030f66dbd
Joseph-Simon <36781112+Joseph-Simon@users.noreply.github.com>
2018-03-23 10:04:52 -0400

    New JS file with array
    Added a new file with an array of quote obj

---
+++ js/quotes.js
@@ -0,0 +1,43 @@
+var quotes = [
+{
+    quote:"My Mama always said, 'Life was like a b ox of chocolates; you never know what you're going to get",
+    source:"Forrest Gump",
+    citation: "in Forrest Gump"
+},
+{
+    quote:"Go ahead make my day",
+    source:"Harry Callahan",
+    citation: "Sudden Impact"
+},
+{
+    quote:"The greatest trick the Devil ever pulled was convincing the world he didn't exist",
+    source:"Roger Kint",
+    citation: "The Usual Suspects"
+},
+{
+    quote:"Go ahead make my day",
+    source:"Harry Callahan",
+    citation: "Sudden Impact"
+},
+{
+    quote:"I'll be back",
+    source:"T-800 Model 101",
+    citation: "Terminator"
+},
+{
+    quote:"As you wish",
+    source:"Westley",
+    citation: "The Princess Bride"
+},
+{
+    quote:"We're all pretty bizarre, Some of us are just better at hiding it, thats all",
+    source:"Andrew Clark",
+    citation: "The Breakfast club"
+},
+{
+    quote:"Screws fall out all the time, the world is an imperfect place",
+    source:"John Bender",
+    citation: "The Breakfast club"
+}
+
+];
